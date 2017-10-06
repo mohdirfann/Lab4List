84345e99dc09ece64ea1739b82c44da4417657e9
Mohd Irfan <mohdirfann77@gmail.com>
2017-10-06 12:50:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lab4list/FullImageActivity.java
@@ -0,0 +1,27 @@
+package com.example.lab4list;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+/**
+ * Created by student24 on 10/2/17.
+ */
+
+public class FullImageActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.full_image);
+
+        Intent i=getIntent();
+
+        int position=i.getExtras().getInt("id");
+        ImageAdapter imageAdapter=new ImageAdapter(this);
+
+        ImageView imageView=(ImageView) findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.mThumbIds[position]);
+    }
+}
